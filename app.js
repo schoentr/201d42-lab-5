e3a13e7e9bf8c20075531f752ef6141614303a89
Tim Schoen <schoentr@gmail.com>
2018-11-30 19:14:50 -0800

    Problem5 solved

--- app.js
+++ app.js
@@ -10,7 +10,6 @@ Test this function by hand in the console to get it working, and when you think 
 // Write your code here
 function sum(a, b) { 
   var sum = a + b;
-  
   var message = 'The sum of ' + a + ' and ' + b + ' is ' + sum + '.';
 
   return [sum, message];
@@ -113,11 +112,19 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiplyArray(multArr) { //eslint-disable-line
-
+  console.log(multArr);
+  var prod1= multiply(multArr[0],multArr[1]);
+  console.log(prod1);
+  prod1= multiply(prod1[0],multArr[2]);
+  console.log(prod1);
+  var message1 = 'The numbers '+multArr[0] + ',' + multArr[1]+',' + multArr[2]+' have a product of ' + prod1[0] + '.';
+  console.log(message1);
+  return [prod1[0],message1];
 }
 
+
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyArray(testArray);
+ testMultiplyArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
 
